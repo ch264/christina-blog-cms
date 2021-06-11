31bdce82e428133e87e4df8744a10024484be454
ch264 <christinavhastenrath@gmail.com>
2021-06-11 13:27:19 -0700

    added favicon and emojis in header

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Footer from '../components/footer'
+import { Linkedin, GitHub, Twitter } from 'react-feather';
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -24,7 +25,15 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="container mx-auto p-20" data-is-root-path={isRootPath}>
-      <header className="mb-8">{header}</header>
+      <header className="mb-8 flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between">
+        <div className="mb-2 sm:mb-0 inner">{header}</div>
+        <div className="flex sm:mb-0 self-center post-meta-share-icons">
+          <a href="https://www.linkedin.com/in/christinahastenrath/" className="pr-1" target="_blank" rel="noopener noreferrer"><Linkedin strokeWidth={1.00} /></a>
+          <a href="https://twitter.com/etTinchen" className="pr-1" target="_blank" rel="noopener noreferrer"><Twitter strokeWidth={1.00} /></a>
+          {/* <a href="www.linkedIn.com/in/christinahastenrath" target="_blank" rel="noopener noreferrer">Medium</a> */}
+          <a href="https://github.com/ch264" target="_blank" rel="noopener noreferrer"><GitHub strokeWidth={1.00} /></a>
+        </div>
+      </header>
       <main>{children}</main>
       <Footer /> 
     </div>
