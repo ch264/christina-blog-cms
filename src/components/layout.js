9d8921624b6c2941f3604716d9537f07c4a15955
ch264 <christinavhastenrath@gmail.com>
2021-06-12 13:23:11 -0700

    add icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,9 @@ import { Link } from "gatsby"
 import Footer from '../components/footer'
 import { Linkedin, GitHub, Twitter } from 'react-feather';
 
+// import stackoverflow from '../assets/stackoverflow.svg'
+// import medium from '../assets/medium.svg'
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
@@ -28,10 +31,16 @@ const Layout = ({ location, title, children }) => {
       <header className="mb-8 flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between">
         <div className="mb-2 sm:mb-0 inner">{header}</div>
         <div className="flex sm:mb-0 self-center post-meta-share-icons">
+          {/* <a href="" target="_blank" rel="noopener noreferrer" className="icon"><img className="h-10 p-1" src={stackoverflow} alt="stackoverflow" /></a>
+          
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon"><img className="h-10 p-1" src={medium} alt="medium" /></a> */}
+
           <a href="https://www.linkedin.com/in/christinahastenrath/" className="pr-1" target="_blank" rel="noopener noreferrer"><Linkedin strokeWidth={1.00} /></a>
+
           <a href="https://twitter.com/etTinchen" className="pr-1" target="_blank" rel="noopener noreferrer"><Twitter strokeWidth={1.00} /></a>
-          {/* <a href="www.linkedIn.com/in/christinahastenrath" target="_blank" rel="noopener noreferrer">Medium</a> */}
+  
           <a href="https://github.com/ch264" target="_blank" rel="noopener noreferrer"><GitHub strokeWidth={1.00} /></a>
+          <i className="fab fa-stack-overflow"></i>
         </div>
       </header>
       <main>{children}</main>
